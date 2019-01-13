5d0e7691891432e64c95be4662b1c3b75d1c402a
CR_Avila <carlosrobertoavilah@gmail.com>
2019-01-13 11:42:44 -0600

    Change cornify source

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ window.addEventListener('keyup', (e => {
     pressed.push(e.key);
     pressed.splice(-secretCode.length -1, pressed.length - secretCode.length);
     if(pressed.join('').includes(secretCode)) {
-        console.log('fuck');
         cornify_add();
     };
 }));
